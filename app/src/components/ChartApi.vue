8c14fcfe8d09a9a7b3f020e425f71849ef8ea474
taskdire <136046239+taskdire@users.noreply.github.com>
2025-03-04 12:21:52 -0500

    awd
    awd

---
+++ app/src/components/ChartApi.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+const url = "https://data.cityofnewyork.us/resource/h9gi-nx95.json";
+
+async function getdata() {
+    try {
+        const response = await fetch(url);
+        if (response.status != 200) {
+            throw new Error(response);
+        } else {
+            const data = await response.json();
+            const apidata = data.data;
+            console.log(data.data);
+        }
+    } catch (error) {
+        console.log(error);
+        alert("Sorry, could not find this.");
+    }
+}
+
+getdata();
+</script>
+
+<style lang="scss" scoped></style>
