20e881493a5b855ec9f1ad73971cc8663a42c567
taskdire <136046239+taskdire@users.noreply.github.com>
2025-03-17 12:25:50 -0400

    aawd
    awddwa

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
@@ -11,6 +12,14 @@ export default [
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
   },
 
   js.configs.recommended,
