d6df1a3a91448199f760ac837f31c6fd5643a593
taskdire <136046239+taskdire@users.noreply.github.com>
2025-03-17 12:33:22 -0400

    awd
    awd

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,33 @@
+<template>
+  <div>
+    <div v-for="(car, index) in cars" :key="index" :car="car" :id="index + 1">
+      {{ car.vehicle_type_code1 }}
+
+    </div>
+
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+const url = "https://data.cityofnewyork.us/resource/h9gi-nx95.json";
+
+async function getdata() {
+  try {
+    const response = await fetch(url);
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      const apidata = data.data;
+      console.log(apidata);
+    }
+  } catch (error) {
+    console.log(error);
+    alert("Sorry, could not find this.");
+  }
+}
+
+getdata();
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped></style>
