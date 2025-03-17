df22a1bfeaef4108547408e1f89f739032989e69
taskdire <136046239+taskdire@users.noreply.github.com>
2025-03-17 12:10:38 -0400

    Revert "awd"
    This reverts commit 7bc454909237c485c98f0256856d4d31e07df28c.

--- app/src/components/ChartApi.vue
+++ app/src/components/ChartApi.vue
@@ -1,30 +1,29 @@
 <template>
     <div>
-        <div v-for="(car, index) in cars" :key="index" :car="car" :id="index + 1">{{ car.vehicle_type_code1 }}</div>
 
     </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from `vue`
+const url = "https://data.cityofnewyork.us/resource/h9gi-nx95.json";
 
-const cars = ref([])
-
-async function getcar() {
+async function getdata() {
     try {
-        const response = await fetch("https://data.cityofnewyork.us/resource/h9gi-nx95.json")
-        let data = await response.json()
-        cars.value = data
-        console.log(data)
+        const response = await fetch(url);
+        if (response.status != 200) {
+            throw new Error(response);
+        } else {
+            const data = await response.json();
+            const apidata = data.data;
+            console.log(apidata);
+        }
     } catch (error) {
-        console.error('error fetching data:', error)
+        console.log(error);
+        alert("Sorry, could not find this.");
     }
 }
-onMounted(() => {
-    getcar()
-})
 
-
+getdata();
 </script>
 
 <style lang="scss" scoped></style>
