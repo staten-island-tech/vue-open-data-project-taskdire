2eb76bad9534f6b9aa0e2daee23a2c0708e1b243
taskdire <136046239+taskdire@users.noreply.github.com>
2025-03-17 12:05:42 -0400

    Revert "wad"
    This reverts commit 3e5324f35254cd47f594f3886e51cf5345d80339.

---
+++ app/src/components/ChartApi.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div v-for="(car, index) in cars" :key="index" :car="car" :id="index + 1">{{ car.vehicle_type_code1 }}</div>
+
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from `vue`
+
+const cars = ref([])
+
+async function getcar() {
+    try {
+        const response = await fetch("https://data.cityofnewyork.us/resource/h9gi-nx95.json")
+        let data = await response.json()
+        cars.value = data
+        console.log(data)
+    } catch (error) {
+        console.error('error fetching data:', error)
+    }
+}
+onMounted(() => {
+    getcar()
+})
+
+
+</script>
+
+<style lang="scss" scoped></style>
